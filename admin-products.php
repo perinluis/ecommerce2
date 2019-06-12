b0422763deb549c90850d1d5a721594b0d9dfe35
Luis Perin <luis.perin@hotmail.com>
2019-06-12 00:00:54 -0300

    Admin-Produtos

---
+++ admin-products.php
@@ -0,0 +1,110 @@
+<?php 
+
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Products;
+
+$app->get("/admin/products", function(){
+
+	User::verifyLogin();
+
+	$products = Products::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products", array(
+		"products"=>$products
+
+	));
+
+
+});
+
+$app->get("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-create");
+
+
+});
+
+$app->post("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$products = new Products();
+
+	$products->setData($_POST);
+
+	$products->save();
+
+	header("Location: /admin/products");
+	exit;
+
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Products();
+
+	$product->get((int)$idproduct);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-update", array(
+		"product"=>$product->getValues()
+
+
+	));
+
+
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Products();
+
+	$product->get((int)$idproduct);
+
+	$product->setData($_POST);
+
+	$product->save();
+
+	$product->setPhoto($_FILES["file"]);
+
+	header("Location: /admin/products");
+	exit();
+
+
+});
+
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Products();
+
+	$product->get((int)$idproduct);
+
+	$product->delete();
+
+	header("Location: /admin/products");
+	exit();
+
+
+});
+
+
+
+
+
+ ?>
